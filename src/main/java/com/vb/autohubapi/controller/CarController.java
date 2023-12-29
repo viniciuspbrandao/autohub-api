40d9d134f822a3fe6a7650cb1fed45d30bc08b93
Vinicius Brandao <viniciusbrandao@vb4web.com>
2023-12-29 14:54:41 -0300

    feat: adiciona metodo PUT para atualizar registros existentes

--- src/main/java/com/vb/autohubapi/controller/CarController.java
+++ src/main/java/com/vb/autohubapi/controller/CarController.java
@@ -9,6 +9,8 @@ import org.springframework.http.ResponseEntity;
 
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Optional;
+
 @RestController
 @RequestMapping("/carros")
 public class CarController {
@@ -34,6 +36,27 @@ public class CarController {
         repository.save(newCar);
         System.out.println("Carro salvo com sucesso: " + newCar);
         return ResponseEntity.ok().build(); //Qd nao tem corpo no retorno precisa do ".ok().build()"
+    }
+
+    @PutMapping
+    @Transactional
+    public ResponseEntity updateCar(@RequestBody @Valid CarDTO carDTO){
+        System.out.println("Recebendo dados do carro: " + carDTO); //log de depuração
+        //service
+        Optional<CarEntity> optionalCar = repository.findById(carDTO.id());
+        if (optionalCar.isPresent()){
+            CarEntity updatedCar = optionalCar.get(); //ERREI aqui, ao usar o ".findById", precisa do "Optional<>", do ".isPresent()" e do ".get()"
+            updatedCar.setMarca(carDTO.marca());
+            updatedCar.setModelo(carDTO.modelo());
+            updatedCar.setAno(carDTO.ano());
+            updatedCar.setPreco(carDTO.preco());
+            updatedCar.setCor(carDTO.cor());
+            System.out.println("dados inseridos: " + updatedCar); //log de depuração
+            return ResponseEntity.ok().build();
+        }
+        else {
+            return ResponseEntity.notFound().build();
+        }
     }
 
 }
