f123fa427a2efb6e126608bf5091edfb5582f2d0
Vinicius Brandao <viniciusbrandao@vb4web.com>
2023-12-29 14:44:51 -0300

    feat: adiciona metodo POST para criar novos registros

--- src/main/java/com/vb/autohubapi/controller/CarController.java
+++ src/main/java/com/vb/autohubapi/controller/CarController.java
@@ -1,6 +1,9 @@
 package com.vb.autohubapi.controller;
 
+import com.vb.autohubapi.domain.CarEntity;
 import com.vb.autohubapi.repository.CarRepository;
+import jakarta.transaction.Transactional;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 
@@ -21,6 +24,16 @@ public class CarController {
         System.out.println("Listagem de carros concluida. Total de carros: " + allCars.size());
         return ResponseEntity.ok(allCars);
 
+    }
+
+    @PostMapping
+    @Transactional
+    public ResponseEntity insertCar(@RequestBody @Valid CarDTO carDTO){
+        System.out.println("Recebendo dados do carro: " + carDTO);
+        CarEntity newCar = new CarEntity(carDTO);
+        repository.save(newCar);
+        System.out.println("Carro salvo com sucesso: " + newCar);
+        return ResponseEntity.ok().build(); //Qd nao tem corpo no retorno precisa do ".ok().build()"
     }
 
 }
