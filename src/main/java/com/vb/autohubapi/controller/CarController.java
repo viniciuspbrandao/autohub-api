3eb5af62be916172df4fb0d9d9ad94a374686f0e
Vinicius Brandao <viniciusbrandao@vb4web.com>
2023-12-29 14:29:30 -0300

    feat: add metodo getAll para recuperar todos os registros

--- src/main/java/com/vb/autohubapi/controller/CarController.java
+++ src/main/java/com/vb/autohubapi/controller/CarController.java
@@ -1,9 +1,26 @@
 package com.vb.autohubapi.controller;
 
+import com.vb.autohubapi.repository.CarRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/carros")
 public class CarController {
+
+    @Autowired
+    private CarRepository repository;
+
+    @GetMapping
+    public ResponseEntity listAllCars() {
+
+        System.out.println("Entrando no metodo listAllCars");
+        var allCars = repository.findAll();
+        System.out.println("Listagem de carros concluida. Total de carros: " + allCars.size());
+        return ResponseEntity.ok(allCars);
+
+    }
 
 }
