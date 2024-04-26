b130940f51f7c027e8d3a8fbdaaac4c33a3f27de
Vinicius Brandao <viniciusbrandao@vb4web.com>
2024-04-26 10:35:00 -0300

    swagger

--- src/main/java/com/vb/autohubapi/controller/CarController.java
+++ src/main/java/com/vb/autohubapi/controller/CarController.java
@@ -2,6 +2,9 @@ package com.vb.autohubapi.controller;
 
 import com.vb.autohubapi.domain.CarEntity;
 import com.vb.autohubapi.service.CarService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -16,12 +19,14 @@ public class CarController {
     private CarService service;
 
 
+    @Operation(summary = "List all active cars")
     @GetMapping
     public ResponseEntity listAllCarsActives() {
         var activeCars = service.getAllActiveTrue();
         return ResponseEntity.ok(activeCars);
     }
 
+    @Operation(summary = "Create a new car")
     @PostMapping
     public ResponseEntity insertCar(@RequestBody @Valid CarDTO carDTO) {
         System.out.println("Recebendo dados do carro: " + carDTO);
@@ -30,18 +35,26 @@ public class CarController {
         return ResponseEntity.ok(newCar); //Qd nao tem corpo no retorno precisa do ".ok().build()"
     }
 
+    @Operation(summary = "Update a car")
     @PutMapping("/{id}")
     public ResponseEntity updateCar(@PathVariable Long id, @RequestBody @Valid CarDTO carDTO) {
         CarEntity updtCar = service.updateCar(id, new CarEntity(carDTO));
         return ResponseEntity.ok(updtCar);
     }
 
+    @Operation(summary = "Deactivate/Delete a car")
     @DeleteMapping("/{id}")
     public ResponseEntity deleteCar(@PathVariable Long id) {
         CarEntity desactiviedCar = service.disableCarById(id);
         return ResponseEntity.noContent().build();
     }
 
+    @Operation(summary = "Get car by id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Found the car"),
+//            @ApiResponse(responseCode = "201", description = "Car created"),
+            @ApiResponse(responseCode = "400", description = "Invalid id supplied"),
+            @ApiResponse(responseCode = "404", description = "Car not found")})
     @GetMapping("/{id}")
     public ResponseEntity getById(@PathVariable Long id) {
         CarEntity carEntity = service.getCarById(id);
