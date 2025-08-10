3b089330548d36de60f931113822067653929e75
viniciuspbrandao <brandao.vinicius@outlook.com>
2025-08-09 21:22:23 -0300

    Add profile aplication

--- src/main/java/com/vb/autohubapi/AutohubApiApplication.java
+++ src/main/java/com/vb/autohubapi/AutohubApiApplication.java
@@ -1,17 +1,30 @@
 package com.vb.autohubapi;
 
 import lombok.extern.slf4j.Slf4j;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import org.springframework.core.env.Environment;
+import jakarta.annotation.PostConstruct;
 
 @Slf4j
 @SpringBootApplication
 public class AutohubApiApplication {
+
+	@Autowired
+    private Environment env;
 
 	public static void main(String[] args) {
 		log.info("Starting AutohubApiApplication");
 
 		SpringApplication.run(AutohubApiApplication.class, args);
 	}
+
+	@PostConstruct
+    public void printActiveProfiles() {
+        System.out.println("Active profiles: " + String.join(", ", env.getActiveProfiles()));
+    }
 
 }
